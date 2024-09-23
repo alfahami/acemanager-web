716618f3001b9f62099e11fd70aa4bf3921f9d80
AL-FAHAMI <altoihir@gmail.com>
2024-09-23 16:09:14 +0100

    test: add get Members test

--- src/test/java/com/acem/acemanager/AcemanagerTest.java
+++ src/test/java/com/acem/acemanager/AcemanagerTest.java
@@ -1,5 +1,43 @@
 package com.acem.acemanager;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.acem.acemanager.pojo.Member;
+import com.acem.acemanager.repository.AcemRepository;
+import com.acem.acemanager.service.AcemService;
+
+@RunWith(MockitoJUnitRunner.class)
 public class AcemanagerTest {
-    
+    // Tet all business
+
+    @Mock
+    private AcemRepository acemRepository;
+
+    @InjectMocks
+    private AcemService acemService;
+
+    @Test
+     public void getMembersFromRepoTest() {
+        when(acemRepository.getMembers()).thenReturn(Arrays.asList(
+            new Member("NBE388507", "Tupac", "Shakur", "altoihir@gmail.com", 20111473, true),
+            new Member("NBE388597", "Notorius", "Biggie", "ntbig@gmail.com", 20115473, false)
+        ));
+
+        List<Member> result = acemRepository.getMembers();
+
+        assertEquals("NBE388507", result.get(0).getPassport());
+        assertEquals(false, result.get(1).getIsMember());
+     }
+
 }
